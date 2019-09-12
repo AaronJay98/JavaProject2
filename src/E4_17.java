c3e68a48122f855322e03707eec5ed479bc2d8ab
Aaron Jay Macaldo <Aaronjay.macaldo@sjsu.edu>
2019-09-12 00:20:38 -0700

    Fixed a bug in project E4_17 when hours were both 0 and the minutes of first time was larger than second time

--- src/E4_17.java
+++ src/E4_17.java
@@ -12,13 +12,30 @@ public class E4_17 {
         int iMinutes2 = 0;      //This will be the minutes portion of the second time
         int iDiffHours = 0;     //This will be the total difference in hours between time 1 and time 2
         int iDiffMinutes = 0;   //This will be the total difference in minutes between time 1 and time 2
+        boolean bNoInputError = false;
 
         System.out.println("Welcome to my program that prints the number of hours and minutes between two times \n" + //This will print the welcoming statement explaining purpose of program
                            "Please ONLY use military format (ex. 0900, 1730)");    //This will help user on how to input time correctly
-        System.out.println("Please enter the first time: ");    //This will print a message asking user to input the first time
-        iTime1 = sUserInput.nextInt();                          //This will receive the user input and store into iTime1 as an Int
-        System.out.println("Please enter the second time: ");   //This will print a message asking user to input the second time
-        iTime2 = sUserInput.nextInt();                          //This will receive the user input and store into iTime2 as an Int
+
+        do {
+            System.out.println("Please enter the first time: ");    //This will print a message asking user to input the first time
+            iTime1 = sUserInput.nextInt();                          //This will receive the user input and store into iTime1 as an Int
+            System.out.println("Please enter the second time: ");   //This will print a message asking user to input the second time
+            iTime2 = sUserInput.nextInt();                          //This will receive the user input and store into iTime2 as an Int
+
+            if(iTime1 < 0 || iTime1 >= 2400 || iTime2 < 0 || iTime2 >= 2400) {      //Checks if first time is within valid bounds
+                System.out.println("You have entered a number that is not a valid military format time. Must be in between 0000 and 2359 inclusive. Please try again. \n");
+                bNoInputError = true;                                               //Sets boolean to true to loop again since error is found
+            }
+            else if((iTime1 % 100) >= 60 || (iTime2 % 100) >= 60) {                 //Checks to see that minutes portion is under 60
+                System.out.println("You have entered a number that is not a valid military format time. The last two digits (minutes) must be under 60. Please try again. \n");
+                bNoInputError = true;                                               //Sets boolean to true to loop again since error is found
+            }
+            else {
+                bNoInputError = false;      //If no error sets boolean to false to exit loop
+            }
+
+        } while(bNoInputError);             //Check if error occured
 
         iHours1 = iTime1 / 100;       //This will get the hours portion of the first time and store into the appropriate hours variable
         iHours2 = iTime2 / 100;       //This will get the hours portion of the second time and store into the appropriate hours variable
@@ -36,10 +53,14 @@ public class E4_17 {
             iDiffMinutes = iMinutes2 - iMinutes1;               //If true : simply subtract minutes of time 2 by minutes of time 1
         }
         else {                                                  //If false :
-            iDiffHours -= 1;                                    //Decrements the difference in hours to "borrow" for the minutes
+            if(iDiffHours != 0) {                                   //Checks if diff in hours is over 0
+                iDiffHours--;                                       //If true : decrements idiffhours
+            }
+            else {
+                iDiffHours = 23;                                    //If false : sets it to 23 (0 is the same as 24 and 24-1 is 23)
+            }
             iDiffMinutes = (60 - iMinutes1) + iMinutes2;        //Gets the remaining minutes to get to next hour and adds the second number
-                                                                //to get the difference in minutes
-        }
+        }                                                       //to get the difference in minutes
 
         System.out.println("The number of hours and minutes between the two times is: " + iDiffHours + " hours " + iDiffMinutes + " minutes");
     }
